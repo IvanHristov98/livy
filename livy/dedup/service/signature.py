259a65f5e403f5513509b488e45db4b894c1b2a6
Ivan Hristov <ivan.hristov@siman.tech>
2023-05-11 22:36:39 +0300

    Build graph correctly

--- livy/dedup/service/signature.py
+++ livy/dedup/service/signature.py
@@ -1,4 +1,4 @@
-from typing import List, NamedTuple, Dict
+from typing import List, NamedTuple, Dict, Set
 import math
 
 import hdbscan
@@ -13,40 +13,74 @@ import livy.dedup.service as service
 class WeightedMedoid(NamedTuple):
     # medoid is the most centrally located element of a cluster.
     medoid: np.ndarray
-    # weight is between 0 and 1.
-    weight: float
+    # weight is an unsigned integer.
+    weight: int
 
-DiscreteHistogram = List[int]
+
+class Node(NamedTuple):
+    resource: int
+
+
+class Edge(NamedTuple):
+    to: int
+    # The graph should be directed and weakly connected.
+    # This is a helper field that says whether an edge is present only in the directed graph.
+    strong: bool
+    cost: int
+
+
+class Graph(NamedTuple):
+    # Invariant states that the number of adj_lists is equal to the number of nodes.
+    adj_list: List[List[Edge]]
+    nodes: List[Node]
+
+
+class SpanningTreeNode:
+    idx: int
+    children: List['SpanningTreeNode']
+
+    def __init__(self, idx: int, children: List['SpanningTreeNode']) -> None:
+        self.idx = idx
+        self.children = children
 
 
 class SignatureService(service.Service):
+    ROOT_IDX = 0
+
     # Hyper-parameters.
     _min_cluster_size: int
     _credits: int
 
     _extractor: service.Extractor
-    _discrete_signatures: Dict[id.Image, DiscreteHistogram]
+    _signatures: Dict[id.Image, WeightedMedoid]
     _ims: Dict[id.Image, model.Image]
 
     def __init__(self, extractor: service.Extractor, min_cluster_size: int = 5, credits: int = 1000000) -> None:
         self._extractor = extractor
-        self._discrete_signatures = []
+        self._signatures = dict()
+        self._ims = dict()
 
         self._min_cluster_size = min_cluster_size
         self._credits = credits
 
     def add_im(self, im: model.Image) -> id.Image:
         weighted_medoids = self._signature(im)
-        discrete_signature = self._discrete_signature(weighted_medoids)
 
-        self._discrete_signatures[im.id] = discrete_signature
+        self._signatures[im.id] = weighted_medoids
         self._ims[im.id] = im
 
     def im(self, id: id.Image) -> model.Image:
         return self._ims[id]
 
     def similar_ims(self, im: model.Image, n: int) -> List[id.Image]:
-        pass
+        signature = self._signature(im)
+
+        for im_id in self._signatures.keys():
+            other_signature = self._signatures[im_id]
+
+            graph = self._build_graph(signature, other_signature)
+
+
 
     # The sum of weights of all weighted medoids should approximate 1.
     def _signature(self, im: model.Image) -> List[WeightedMedoid]:
@@ -91,51 +125,88 @@ class SignatureService(service.Service):
                 medoids[labels[i]] = features[i]
                 medoid_dists[labels[i]] = dist
 
-        # Find the weighted medoids.
+        # Calculating weight.
+        # The more credits, the better the accuracy.
+        left = self._credits
         weighted_medoids = []
 
         for label in medoids.keys():
             weight = cluster_sizes[label] / used_features_cnt
-            weighted_medoid = WeightedMedoid(medoid=medoids[label], weight=weight)
+            discrete_weight = math.floor(self._credits * weight)
 
+            if len(weighted_medoids) == len(medoids.keys()) - 1:
+                discrete_weight = left
+            
+            left -= discrete_weight
+
+            weighted_medoid = WeightedMedoid(medoid=medoids[label], weight=discrete_weight)
             weighted_medoids.append(weighted_medoid)
 
         return weighted_medoids
 
-    def _discrete_signature(self, signature: List[WeightedMedoid]) -> List[DiscreteHistogram]:
-        # The more credits, the better the accuracy.
-        left = self._credits
-        hists = [None] * len(signature)
+    # The number of credits in the supplier signature should be equal ot those in the consumer signature.
+    def _build_graph(self, supplier: List[WeightedMedoid], consumer: List[WeightedMedoid]) -> Graph:
+        graph = Graph(adj_list=[], nodes=[])
 
-        for i in range(len(signature)):
-            medoid = signature[i]
-            # Handle credits.
-            credits = math.floor(self._credits * medoid.weight)
+        # In order to apply the network simplex algorithm a dummy root is needed.
+        # The simplex algorithm basis is represented through a spanning tree and
+        # a bipartite graph with edges only in one direction has no spanning tree.
+        root = Node(resource=0)
+        graph.adj_list.append([])
+        graph.nodes.append(root)
 
-            if i == len(signature)-1:
-                credits = left
+        count = 1
 
-            left -= credits
+        # Number of clusters is usually small (e.g. 10).
+        # Hence number of iterations is around 5 * 400 = 2000 nodes.
+        for medoid in supplier:
+            graph.adj_list.append([])
+            graph.nodes.append(Node(resource=medoid.weight))
 
-            hists[i] = self._discrete_histogram(medoid, credits)
+            # Connect to root.
+            graph.adj_list[self.ROOT_IDX].append(Edge(to=count, strong=True, cost=0))
+            # Achieving weak connectivity.
+            graph.adj_list[count].append(Edge(to=self.ROOT_IDX, strong=False, cost=0))
 
-        return hists
+            count += 1
 
-    def _discrete_histogram(self, medoid: WeightedMedoid, credits: int) -> DiscreteHistogram:
-        total = 0.0
+        for i in range(len(consumer)):
+            consumer_medoid = consumer[i]
 
-        for val in medoid.medoid:
-            total += val
+            graph.adj_list.append([])
+            graph.nodes.append(Node(resource=consumer_medoid.weight))
 
-        left = credits
-        hist = [0] * len(medoid.medoid)
+            for j in range(len(supplier)):
+                supplier_medoid = supplier[j]
 
-        for i in range(len(medoid.medoid)):
-            ratio = medoid.medoid[i]/total
-            hist[i] = math.floor(credits * ratio)
-            if i == len(medoid.medoid) - 1:
-                hist[i] = left
+                # +1 is added because the first node is the root.
+                dist = linalg.norm(supplier_medoid.medoid - consumer_medoid.medoid)
+                graph.adj_list[j+1].append(Edge(to=count, strong=True, cost=dist))
+                graph.adj_list[count].append(Edge(to=j+1, strong=False, cost=-dist))
 
-            left -= hist[i]
+            count += 1
 
-        return hist
+        return graph
+
+    def _find_optimal_flow_matrix(self, graph: Graph) -> List[List[float]]:
+        root = self._spanning_tree(graph)
+
+    def _spanning_tree(self, graph: Graph) -> SpanningTreeNode:
+        def _aux_spanning_tree(node: SpanningTreeNode, visited: Set[int]) -> None:
+            neighbours = graph.adj_list[node.idx]
+
+            for neighbour_edge in neighbours:
+                if neighbour_edge.to in visited:
+                    continue
+
+                child_node = SpanningTreeNode(idx=neighbour_edge.to, children=[])
+                node.children.append(child_node)
+
+                visited.add(child_node.idx)
+                _aux_spanning_tree(child_node, visited)
+
+        root = SpanningTreeNode(idx=self.ROOT_IDX, children=[])
+        visited = set([self.ROOT_IDX])
+
+        _aux_spanning_tree(root, visited)
+        return root
